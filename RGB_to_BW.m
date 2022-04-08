edeb2ea6062fde8261074b518707a6be2e14ddba
arosard <alexandre@rosard.info>
2022-04-08 11:48:22 +0200

    symplifying RGB_to_BW

--- RGB_to_BW.m
+++ RGB_to_BW.m
@@ -1,23 +1,13 @@
-function RGB_to_BW(path_origin,boolean, path_end)
+function RGB_to_BW(name_origin)
+path_origin = "raw_data/"+name_origin;
+path_end = "treated_data/bw_"+name_origin;
 
 photo = imread(path_origin);
-
-if boolean 
-    BW_image = imbinarize(photo,'adaptive','ForegroundPolarity','dark','Sensitivity',0.55);
-else %if the image is pre-treated
-    BW_image = imbinarize(photo);
-%     BW_image = imfill(BW_image,'holes');
+BW_image = imbinarize(photo,'adaptive','ForegroundPolarity','dark','Sensitivity',0.55);
+%BW_image = bwpropfilt(BW_image,'Area',10,'smallest');
+imwrite(imcomplement(BW_image),path_end);
 end
 
-switch nargin
-    case 2
-        imwrite(imcomplement(BW_image),path_origin);
-    case 3
-        imwrite(imcomplement(BW_image),path_end);
-    otherwise
-      error('Wrong number of inputs.');
-end
-end
 
 
 
