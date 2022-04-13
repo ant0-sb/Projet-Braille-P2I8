deb36979a79138b926a02999ae18a4b31308cc0c
Eleonore12345 <chopin.haydn@laposte.net>
2022-04-13 17:47:39 +0200

    modif d'Alex fonctionnelle

--- les_debuts.m
+++ les_debuts.m
@@ -1,5 +1,5 @@
 clc;
 close all;
-RGB_to_BW("braille_scan.jpg", false, "résultat.jpg") 
-%true seulement pour les vraies photos pas trichées
-imshow("résultat.jpg")
+nom = "braille.jpg";
+RGB_to_BW(nom) 
+imshow("treated_data/bw_"+nom)
