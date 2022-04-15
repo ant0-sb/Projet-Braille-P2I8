6f7c61d6439c2d27e803d86ecb8e3702f7ff7ce6
Eleonore12345 <chopin.haydn@laposte.net>
2022-04-15 11:48:12 +0200

    centroides

--- analyse_image.m
+++ analyse_image.m
@@ -1,11 +1,20 @@
 clc;
 
 load treated_data/bw_braille_scan.jpg
-Im1 = imread('echantillon.jpg','jpeg');
+Im1 = imread('treated_data/bw_braille_scan.jpg','jpeg');
+T = graythresh(Im1)
+BW = im2bw (Im1,T);
 
-BW = im2bw (Im1,0.4);
 imshow(BW, []);
-[label, n] = bwlabel(Im1);
+CC = bwconncomp(BW);
+S = regionprops(CC,'Centroid');
+
+S(2)
+%figure;
+for i=2 : 3049(S)
+    plot(S(i(1)),S(i(2)));
+end
+
 
 %BW=Im1>1000;
 %{
