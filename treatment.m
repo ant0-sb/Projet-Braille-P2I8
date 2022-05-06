f45d5356c875d3f105d0ef8f8d5179fcce806045
ant0-sb <antonin.sibileau@gmail.com>
2022-05-06 11:51:50 +0200

    detection d'une ligne

--- treatment.m
+++ treatment.m
@@ -13,7 +13,21 @@ BW = bwpropfilt(BW,'Area', [0 50]);
 % Equivalent treatment: BW2 = xor(bwareaopen(BW,0),  bwareaopen(BW,50));
 BWzoom = BW (50:1500, 80:1000);
 imwrite(BWzoom,"treated_data/bw_"+name_image+".jpg");
-st = regionprops(BWzoom, 'Centroid');
+st = regionprops(BWzoom, 'Centroid'); %structure = matrice de classe
 c = vertcat(st.Centroid);
+x_centroids = c(:,1);
+y_centroids = c(:,2);
 imshow(BWzoom); hold on;
-line(c(:,1), c(:,2), 'LineStyle','none', 'Marker','+', 'Color','b')
+%Affichage des centroids
+% line(x_centroids,y_centroids, 'LineStyle','none', 'Marker','+', 'Color','b')
+ligne_x = [x_centroids(25)];
+ligne_y = [y_centroids(25)];
+
+for j=(1:length(y_centroids))
+    if abs(y_centroids(25)-y_centroids(j)) < 4
+        ligne_x(end+1) = x_centroids(j);
+        ligne_y(end+1)= y_centroids(j);
+    end
+end
+plot(ligne_x,ligne_y,'LineStyle','none', 'Marker','+', 'Color','b')
+
