0a83d0f2d26da8b202d4e949534b1a57c4bb55f1
arosard <alexandre@rosard.info>
2022-05-17 11:35:59 +0200

    meilleur traitement d'image

--- traitement.m
+++ traitement.m
@@ -1,17 +1,16 @@
-%Objectif : Modification de l'image de orginiale pour permettre plus tard une reconaissance plus facile
-close all
-clear var
+%Objctif : Modification de l'image de orginiale pour permettre plus tard une reconaissance plus facile
 clc
+clear
+close all
 name_image = "braillePierre";
-im0 = imread("raw_data/"+name_image,'jpg');
-if ndims(im0)==3
-    im0 = rgb2gray(im0); 
+I = imread("raw_data/"+name_image,'jpg');
+if ndims(I)==3
+    I = rgb2gray(I); 
 end
-BW = imbinarize(im0, 0.7);
-BW = imcomplement (BW);
-BW = imdilate (BW, strel('disk',1));
-BW = bwpropfilt(BW,'Area', [0 50]);
-% Equivalent treatment: BW2 = xor(bwareaopen(BW,0),  bwareaopen(BW,50));
-BWzoom = BW (50:1500, 80:1000);
-%Attention pour les imwrite, ne pas utiliser le format jpg car il ne supporte pas les " binary images as input arrays".
-imwrite(BWzoom,"treated_data/bw_"+name_image+"tiff"); %If A is of data type logical, then imwrite assumes that the data is a binary image and writes it to the file with a bit depth of 1, if the format allows it. BMP, PNG, or TIFF formats accept binary images as input arrays.
+SE = strel('disk',12);
+I = imbothat(I,SE); % On égalise l'éclairage de la photo
+I = imbinarize(I,"global"); % On tranforme en 0 et 1 avec Otsu
+I = bwareaopen(I,8); % On enlève les petits points de lumières
+SE = strel('line',5,0);
+I = imdilate(I,SE); % On tranforme les ombres en points (carrés)
+imwrite(I,"treated_data/bw_"+name_image+".tif");
