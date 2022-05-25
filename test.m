3a384885c30760490a21be4f7378dc051b2b6500
arosard <alexandre@rosard.info>
2022-05-25 14:55:22 +0200

    nouveau traitement d'image et figure dans le rapport pour le passage en noir et blanc

--- test.m
+++ test.m
@@ -1,31 +1,37 @@
 clc
 clear
 close all
-name_image = "braillePierre";
+name_image = "braille_scan";
 I = imread("raw_data/"+name_image,'jpg');
-if ndims(I)==3
-    I = rgb2gray(I); 
+% Binarisation de l'image
+if ndims(I)==3 % Test si l'image est en RGB
+    I = rgb2gray(I); % Conversion en niveaux de gris 
 end
 SE = strel('disk',12);
-I = imbothat(I,SE); % On égalise l'éclairage de la photo
-I = imbinarize(I,"global"); % On tranforme en 0 et 1 avec Otsu
-I = bwareaopen(I,8); % On enlève les petits points de lumières
-SE = strel('line',5,0);
-I = imdilate(I,SE); % On tranforme les ombres en points (carrés)
+I = imbothat(I,SE); % Égalisation de l'éclairage de la photo
+I = imbinarize(I,"global"); % Binarization avec la méthode d'Otsu
+% Nettoyage de l'image
+I = bwareaopen(I,6); % Suppresion des petits points de lumières
+SE = strel('disk',1,0);
+I = imdilate(I,SE); % Grossissement des points
+I = imresize(I,2); % modification de la résolution (amélioration de la précision)
+D = -bwdist(~I,'cityblock'); % tranformée des distances
+mask = imextendedmin(D,2); %création d'un masque
+D = imimposemin(D,mask);
+L = watershed(D); % Application de la méthode de lignes de partage des eaux
+L(~I) = 0; % Détection des jointures
+I(L == 0) = 0; % Suppression des jointures
 
-st = regionprops(I, 'Centroid'); %extraction des centroid des différents points
-c = vertcat(st.Centroid); % extraction des coordonnées et mise en matrice N*2
-distances = pdist(c); % Matrices des distances entre tous les centroids
-link = linkage(distances,'single');
-clust = cluster(link,'cutoff',60,'criterion','distance'); % Assignation d'un index de cluster à chaque centre
-figure
-scatter(c(:,1),c(:,2),10,clust);
-axis equal
-M = mode(clust); % Obtention de l'index du plus gros cluster
-big_c = zeros(1,2); % Extraction des points du plus gros cluster
-for i=1:length(c)
-    if clust(i) == M
-        big_c=[big_c;c(i,:)];
-    end
+% Rotation minime qui permet de corriger des problèmes d'alignement
+maxAngleParfait = 0; %nombre maximun de point alignés
+angleParfait = 0; % angle de rotation final
+for angle=-2.5:0.05:2.5
+ Irot = imrotate(I,angle); % On effectue une rotation de l'image
+ projectionX = sum(Irot); % On projette l'image sur l'axe Y
+ maxProjection = max(projectionX); % On regarde le maxime de point alignés
+ if maxProjection >= maxAngleParfait % Si plus de points sont alignés
+     maxAngleParfait = maxProjection; % On stocke ce nouveau maximum
+     angleParfait = angle; % Cet angle devient notre meilleur angle
+ end
 end
-[big_c_max,maxIdx]=max()
+I = imrotate(I,angleParfait);
