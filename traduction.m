6ade24c1ef9a6b23dcc60be94b0faa8e7babd2fb
PierreGallinMartel <pierre.gallinmartel@gmail.com>
2022-06-01 22:26:05 +0200

    ajout de commentaires sur le script de traduction

--- traduction.m
+++ traduction.m
@@ -22,11 +22,11 @@ end
 %% traduction des caractères
 swaplist = 0;
 phrase = strings(50); %matrice de strings, nombre défini arbitrairement et à étendre pour des textes plus longs
-k = 1;
-capshift =0;
-exposantunique=false;
-finexposant = false;
-for n = 1:length(braille)
+k = 1; %numéro de la ligne lors de la traduction du texte
+capshift =0; %utilisé lorsque un décalage ASCII est nécessaire
+exposantunique=false; %permet de n'appliquer le signe de l'exposant qu'à un caractère lorsqu'il n'y a pas d'expression encadrée
+finexposant = false; %assure que la parenthèse de l'exposant ne se referme que si elle a été ouverte, utile dans certaines conditons de traduction
+for n = 1:length(braille) %parcours l'ensemble de la phrase en braille à décoder
     % échangeur de listes
     switch swaplist
         case 0
@@ -43,17 +43,17 @@ for n = 1:length(braille)
             liste = listespeciale;
     end
     
-    for lettreref = liste
+    for lettreref = liste %compare une lettre de la phrase en braille à toutes celles de la liste choisie juqu'à détecter une correspondance
         %détermine si la liste doit changer à la prochaine itération de la
         %boucle
         if isequal(braille{n},lettreref.valeur)
-            if (swaplist == 3)
+            if (swaplist == 3) %certaines balises d'interprétation ne sont valables que pour un caractère par convention, on les fait donc stopper ici pour l'itération suivante
                 swaplist = 0;
             elseif (swaplist == 4)
                 swaplist = 1;
             elseif (swaplist == 5)
                 swaplist = 1;
-            elseif (lettreref.caractere =="chiffre")
+            elseif (lettreref.caractere =="chiffre") %ici on teste chaque balise afin de déterminer quelle sera la liste à utiliser pour le prochain symbole à traduire
                 swaplist = 1;
             elseif (lettreref.caractere == ' ')
                 swaplist = 0;
@@ -84,14 +84,13 @@ for n = 1:length(braille)
             
             %décalage ASCII éventuel lors de l'apparition d'une majuscule
             if (lettreref.caractere == "caps")
-                disp (braille{n+1});
                 %empêche le décalage ASCII pour les symboles balisés par le
                 %signe des majuscule, ici les accolades
                 if (~(isequal(braille{n+1},[0,0;1,0;1,1]) || isequal(braille{n+1},[0,0;0,1;1,1])) )
                     capshift = 32;
                 end
             else
-                    if (isequal(braille{n},[[0 1];[0 0];[0 0]]))
+                    if (isequal(braille{n},[[0 1];[0 0];[0 0]])) %permet de déterminer si un seul caractère ou tout une expression est mis en exposant
                         if(~isequal(braille{n+1},[[0 0];[0 1];[0 1]]))
                             exposantunique = true;
                         else
@@ -102,15 +101,15 @@ for n = 1:length(braille)
                     %plusieurs espaces consécutifs sont détectés
                     if (lettreref.caractere == ' ')
                         if(n < length(braille))
-                            if (isequal (braille{n+1}, lettreref.valeur) && ~isequal (braille{n-1}, lettreref.valeur))
+                            if (isequal (braille{n+1}, lettreref.valeur) && ~isequal (braille{n-1}, lettreref.valeur)) %permet de limiter le saut de ligne au premier espace d'une fin de ligne, afin que ceux-ci ne s'enchaînent pas tant qu'un caractère non nul n'est pas détecté
                                 k = k+1;
                             end
                         end
                     end
 
-                    phrase(k,1) = phrase(k,1) + char(double(lettreref.caractere)-capshift);
-                    capshift = 0;
-                    if (isequal(braille{n}, [[0 0];[1 0];[1 0]]))
+                    phrase(k,1) = phrase(k,1) + char(double(lettreref.caractere)-capshift); %ajoute le caractère traduit à la fin de la phrase, avec un décalage ASCII dans les cas des majuscules
+                    capshift = 0; 
+                    if (isequal(braille{n}, [[0 0];[1 0];[1 0]])) %%conditionne l'écriture d'une fin d'exposant, afin que la parenthèe correspondante n'apparaîsse pas à des endroits indésirables
                         if (finexposant)
                             phrase(k,1) = phrase(k,1) + ')';
                             finexposant = false;
