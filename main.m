9dff1635cb22847d48ccb08c61e672d4e1e4a3db
Eleonore12345 <chopin.haydn@laposte.net>
2022-06-01 23:28:25 +0200

    récupération de la traduction dans un fichier txt

--- main.m
+++ main.m
@@ -13,5 +13,10 @@ I_traitee = traitement_d_image(I); %traitement de la photo
 matrice_binaire = reconnaissance_points_braille(I_traitee); %création de l'équivalent de la photo en matrice de 0 et 1
 
 texte = traduction(matrice_binaire); %traduction de la matrice binaire équivalente
-%% Le texte traduit peut être récupéré et lu dans la variable texte.
+%% Le texte traduit peut être récupéré et lu dans le fichier "résultat.txt"
+%% Il est aussi affiché dans le terminal
+close all;
 disp(texte(:,1));
+fid = fopen('résultat.txt','w');
+fprintf(fid, '%s\n', texte);
+fclose(fid);
